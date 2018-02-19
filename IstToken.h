5d58717fd575c1d14e80826abb066bb9b280bad5
cronof <cronof0510@gmail.com>
2018-02-19 14:05:43 +0700

    upDate

---
+++ IstToken.h
@@ -0,0 +1,13 @@
+#define LOAD_IST 0001
+#define ADD_IST 0100
+#define SUB_IST 0101
+#define OR_IST  0110
+#define XOR_IST 0111
+#define BRA_IST 1000
+#define BRAZ_IST 1001
+#define BRAL_IST 1010
+#define BRALZ_IST 1011
+#define CALL_IST 1100
+#define HALT_IST 1110
+#define IN_IST 1101
+#define OUT_IST 1111

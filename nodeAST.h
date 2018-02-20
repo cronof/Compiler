48eccc0bd28873d9753aa4d17a7ac01e81da2490
cronof <cronof0510@gmail.com>
2018-02-20 22:52:47 +0700

    Nhum20/2/2018

--- nodeAST.h
+++ nodeAST.h
@@ -12,19 +12,22 @@ char Block (Statement s){
 }
 ////////Instructions Fucntion/////////////
 
-char f_Load (Statement s){
-
+void f_Load (Statement s){
+  RD = s;
 }
-char  f_Add (Statement s){
-
+char f_Add (Statement s1, Statement s2){
+  RD = s1 + s2;
+  return RD;
 }
-char f_Sub (Statement s){
-
+char f_Sub (Statement s1, Statement s2){
+  RD = s1 - s2;
+  return RD;
 }
-char f_Or (Statement s){
-
+char f_Or (Statement s1, Statement s2){
+  //RD = s1 + s2;
+  return RD;
 }
-char f_Xor (Statement s){
+char f_Xor (Statement s1, Statement s2){
 
 }
 char f_Bra (Statement s){
